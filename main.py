a38bcbee993642e986e1bc75fc87f19bf6fc5770
pyTimK <45229539+pyTimK@users.noreply.github.com>
2023-04-03 09:47:28 +0800

    Added flask apis

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def main():
     # input_name = "naturally_greater_than_one"
     input_name = "DEMO"
     input_json = FileManager.load_xmp(f"./input/{input_name}.xmp")
+    print(input_json)
     snp_system = Snp_system(input_json)
     homogenize(snp_system)
     output_name = f"./output/homogenized_{input_name}.xmp"
